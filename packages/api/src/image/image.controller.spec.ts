fb3d2d2a4a89537700c91fa7ac93591a98a76dbb
Livio <livio.brunner.lb1@gmail.com>
2019-06-21 14:26:11 +0200

    fix(api): Fix ImageController test

--- packages/api/src/image/image.controller.spec.ts
+++ packages/api/src/image/image.controller.spec.ts
@@ -1,125 +1,137 @@
 import { Test } from '@nestjs/testing';
 
-import { ImageDetailDto, ImageListItemDto } from './dtos';
+import {
+  ImageDetailDto,
+  ImageListItemDto,
+  CloneImageResponseDto
+} from './dtos';
 import { ImageController } from './image.controller';
 import { ImageService } from './image.service';
+import { PaginationResponseDto, ResponseDto } from '../common';
 
 class ImageServiceMock {
-    async findByRemote() { }
-    async findOne() { }
-    async cloneImage() { }
-
+  async findByRemote() {}
+  async findOne() {}
+  async cloneImage() {}
 }
 /**
  * Test cases for the image controller
  */
 describe('ImageController', () => {
-    let imageController: ImageController;
-    let imageService: ImageService;
+  let imageController: ImageController;
+  let imageService: ImageService;
 
-    beforeEach(async () => {
-        // Mock Image Module
-        const module = await Test.createTestingModule({
-            controllers: [ImageController],
-            providers: [
-                {
-                    provide: 'ImageService',
-                    useClass: ImageServiceMock
-                }
-            ]
-        }).compile();
+  beforeEach(async () => {
+    // Mock Image Module
+    const module = await Test.createTestingModule({
+      controllers: [ImageController],
+      providers: [
+        {
+          provide: 'ImageService',
+          useClass: ImageServiceMock
+        }
+      ]
+    }).compile();
 
-        // Get the imageController and imageService in the Testing Module Context
-        imageController = module.get<ImageController>(ImageController);
-        imageService = module.get<ImageService>(ImageService);
+    // Get the imageController and imageService in the Testing Module Context
+    imageController = module.get<ImageController>(ImageController);
+    imageService = module.get<ImageService>(ImageService);
+  });
+
+  describe('findByRemote', () => {
+    /**
+     * Request images per remote
+     */
+    it('should return ImageListItems by remote', async () => {
+      const result: ImageListItemDto[] = [
+        {
+          fingerprint: 'fingerprint1',
+          description: 'desc1',
+          uploadedAt: new Date(),
+          id: 1,
+          _links: {
+            detail: 'http://localhost:3000/api/v1/image/1'
+          }
+        },
+        {
+          fingerprint: 'fingerprint2',
+          description: 'desc2',
+          uploadedAt: new Date(),
+          id: 2,
+          _links: {
+            detail: 'http://localhost:3000/api/v1/image/2'
+          }
+        }
+      ];
+
+      const response = new PaginationResponseDto(result, result.length, {
+        limit: 20,
+        offset: 0
+      });
+
+      jest
+        .spyOn(imageService, 'findByRemote')
+        .mockImplementation(() => Promise.resolve(response));
+      expect(
+        await imageController.findByRemote({
+          limit: 20,
+          offset: 0,
+          remote: 'Test'
+        })
+      ).toBe(response);
     });
+  });
 
-    describe('findByRemote', () => {
-        /**
-         * Request images per remote
-         */
-        it('should return ImageListItems by remote', async () => {
-            const result: ImageListItemDto[] = [{
-                fingerprint: 'fingerprint1',
-                description: 'desc1',
-                uploadedAt: new Date(),
-                id: 1,
-                _links: {
-                    detail: 'http://localhost:3000/api/v1/image/1'
-                }
-            }, {
-                fingerprint: 'fingerprint2',
-                description: 'desc2',
-                uploadedAt: new Date(),
-                id: 2,
-                _links: {
-                    detail: 'http://localhost:3000/api/v1/image/2'
-                }
-            }];
+  describe('findOne', () => {
+    it('should return ImageDetail', async () => {
+      const result = {
+        fingerprint: 'fingerprint1',
+        description: 'desc1',
+        uploadedAt: new Date(),
+        id: 1,
+        aliases: [{ description: '', name: '' }],
+        architecture: { humanName: '', processorName: '' },
+        autoUpdate: true,
+        expiresAt: new Date(),
+        label: '',
+        lastUsedAt: new Date(),
+        operatingSystem: { distribution: '', release: '', version: '' },
+        serial: '',
+        createdAt: new Date(),
+        public: true,
+        remotes: [
+          { available: true, cloneable: false, id: 1, name: 'remote1' }
+        ],
+        cloneable: false
+      };
+      const response = new ResponseDto(result as ImageDetailDto);
 
-            jest.spyOn(imageService, 'findByRemote').mockImplementation(() => result);
-            expect(await imageController.findByRemote({ limit: 20, offset: 0, remoteId: 1 })).toBe(result);
-        });
+      jest
+        .spyOn(imageService, 'findOne')
+        .mockImplementation(() => Promise.resolve(response));
+
+      expect(await imageController.findOne('fingerprint1')).toBe(response);
     });
+  });
 
-    describe('findOne', () => {
-        it('should return ImageDetail', async () => {
-            const result: ImageDetailDto = {
-                fingerprint: 'fingerprint1',
-                description: 'desc1',
-                uploadedAt: new Date(),
-                id: 1,
-                aliases: [{ description: '', name: '' }],
-                architecture: { humanName: '', processorName: '' },
-                autoUpdate: true,
-                expiresAt: new Date(),
-                label: '',
-                lastUsedAt: new Date(),
-                operatingSystem: { distribution: '', release: '', version: '' },
-                serial: '',
-                size: 1,
-                createdAt: new Date(),
-                public: true,
-                remotes: [
-                    { available: true, cloneable: false, id: 1, name: 'remote1' }
-                ],
-                cloneable: false
-            };
+  describe('clone', () => {
+    it('should clone an image', async () => {
+      const result = new ResponseDto({ uuid: 'test' } as CloneImageResponseDto);
 
-            jest.spyOn(imageService, 'findOne').mockImplementation(() => result);
+      jest
+        .spyOn(imageService, 'cloneImage')
+        .mockImplementation(() => Promise.resolve(result));
 
-            expect(await imageController.findOne(1)).toBe(result);
-        });
+      expect(
+        await imageController.clone(1, {
+          sourceRemoteId: 1,
+          destinationRemoteId: 2
+        })
+      ).toBe(result);
+      expect(imageService.cloneImage).toHaveBeenCalledWith(1, {
+        sourceRemoteId: 1,
+        destinationRemoteId: 2
+      });
     });
-
-    describe('clone', () => {
-        it('should clone an image', async () => {
-            const result: ImageDetailDto = {
-                fingerprint: 'fingerprint1',
-                description: 'desc1',
-                uploadedAt: new Date(),
-                id: 1,
-                aliases: [{ description: '', name: '' }],
-                architecture: { humanName: '', processorName: '' },
-                autoUpdate: true,
-                expiresAt: new Date(),
-                label: '',
-                lastUsedAt: new Date(),
-                operatingSystem: { distribution: '', release: '', version: '' },
-                serial: '',
-                size: 1,
-                createdAt: new Date(),
-                public: true,
-                remotes: [
-                    { available: true, cloneable: false, id: 1, name: 'remote1' }
-                ],
-                cloneable: false
-            };
-
-            jest.spyOn(imageService, 'cloneImage').mockImplementation(() => result);
-
-            expect(await imageController.clone(1, { sourceRemoteId: 1, destinationRemoteId: 2 })).toBe(result);
-            expect(imageService.cloneImage).toHaveBeenCalledWith(1, { sourceRemoteId: 1, destinationRemoteId: 2 });
-        });
-    });
+  });
 });
