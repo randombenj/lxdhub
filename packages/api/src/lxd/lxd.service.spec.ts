ecc8a9acb5ae528d7df8b17b6023068482c960ab
Livio <livio.brunner.lb1@gmail.com>
2019-06-21 14:03:54 +0200

    fix(api): Fix LxdService test

--- packages/api/src/lxd/lxd.service.spec.ts
+++ packages/api/src/lxd/lxd.service.spec.ts
@@ -4,113 +4,132 @@ import { Test } from '@nestjs/testing';
 import { LXDHubAPISettings } from '..';
 import { SourceImageFactory } from './factories';
 import { LXDService } from './lxd.service';
+import { AxiosToken } from '../third-party';
+import Axios from 'axios';
+import { Agent } from 'https';
 
 class SourceImageFactoryMock {
-    entityToDto() { }
-
+  entityToDto() {}
 }
 
-class RequestMock {
-    async post() { }
-    async get() { }
+class AxiosRequestMock {
+  get() {}
+  post() {}
+}
+class AxiosMock {
+  create() {
+    return new AxiosRequestMock();
+  }
 }
 
 const keyBuffer = new Buffer('key');
 const certBuffer = new Buffer('cert');
 
 const settings: LXDHubAPISettings = {
-    lxd: {
-        key: keyBuffer,
-        cert: certBuffer
-    },
-    database: {
-        database: '',
-        host: '',
-        password: '',
-        port: 3000,
-        username: ''
-    }
+  lxd: {
+    key: keyBuffer,
+    cert: certBuffer
+  },
+  database: {
+    database: '',
+    host: '',
+    password: '',
+    port: 3000,
+    username: ''
+  }
 } as LXDHubAPISettings;
 
 /**
  * Test cases for the remote service
  */
 describe('LXDService', () => {
-    let lxdService: LXDService;
-    let sourceImageFactory: SourceImageFactory;
-    let request: RequestMock;
+  let lxdService: LXDService;
+  let sourceImageFactory: SourceImageFactory;
+  let axios: typeof Axios;
 
-    beforeEach(async done => {
-        // Mock Remote Module
-        const module = await Test.createTestingModule({
-            providers: [
-                {
-                    provide: 'LXDHubAPISettings',
-                    useFactory: () => settings
-                },
-                LXDService,
-                {
-                    provide: 'SourceImageFactory',
-                    useClass: SourceImageFactoryMock
-                },
-                {
-                    provide: 'Request',
-                    useClass: RequestMock
-                }
-            ]
-        }).compile();
+  beforeEach(async done => {
+    // Mock Remote Module
+    const module = await Test.createTestingModule({
+      providers: [
+        {
+          provide: 'LXDHubAPISettings',
+          useFactory: () => settings
+        },
+        LXDService,
+        {
+          provide: 'SourceImageFactory',
+          useClass: SourceImageFactoryMock
+        },
+        {
+          provide: AxiosToken,
+          useClass: AxiosMock
+        }
+      ]
+    }).compile();
 
-        // Get the lxdService in the Testing Module Context
-        lxdService = module.get<LXDService>(LXDService);
-        sourceImageFactory = module.get<SourceImageFactory>(SourceImageFactory);
-        request = module.get<RequestMock>('Request');
-        done();
+    // Get the lxdService in the Testing Module Context
+    lxdService = module.get<LXDService>(LXDService);
+    sourceImageFactory = module.get<SourceImageFactory>(SourceImageFactory);
+    axios = module.get<typeof Axios>(AxiosToken);
+    done();
+  });
+  let image: Image;
+  let remote1: Remote;
+  let remote2: Remote;
+  let sourceImageMock;
+  beforeEach(() => {
+    image = new Image();
+    remote1 = new Remote();
+    remote2 = new Remote();
+    remote2.serverUrl = 'https://destination.com';
+    sourceImageMock = {};
+  });
+
+  describe('cloneImage', () => {
+    it('should correctly call the sourceimage factory ', async () => {
+      const axiosRequest = new AxiosRequestMock();
+      jest
+        .spyOn(sourceImageFactory, 'entityToDto')
+        .mockImplementation(() => sourceImageMock);
+      jest.spyOn(axios, 'create').mockImplementation(() => axiosRequest as any);
+      jest
+        .spyOn(axiosRequest, 'post')
+        .mockImplementation(() =>
+          Promise.resolve({ data: { metadata: { id: '/1' } } })
+        );
+
+      await lxdService.cloneImage(image, remote1, remote2);
+      expect(axiosRequest.post).toHaveBeenCalledWith(
+        'https://destination.com/1.0/images', sourceImageMock,
+      );
+      expect(sourceImageFactory.entityToDto).toHaveBeenCalledWith(
+        image,
+        remote1,
+        remote2
+      );
     });
-    let image: Image;
-    let remote1: Remote;
-    let remote2: Remote;
-    let sourceImageMock;
-    beforeEach(() => {
-        image = new Image();
-        remote1 = new Remote();
-        remote2 = new Remote();
-        remote2.serverUrl = 'https://destination.com';
-        sourceImageMock = {};
+  });
+
+  describe('getCloneStatus', () => {
+    it('should correctly create requests ', async () => {
+      const axiosRequest = new AxiosRequestMock();
+      jest.spyOn(axios, 'create').mockImplementation(() => axiosRequest as any);
+      jest
+        .spyOn(axiosRequest, 'post')
+        .mockImplementation(() =>
+          Promise.resolve({ data: { metadata: { id: '/1' } } })
+        );
+      jest
+        .spyOn(axiosRequest, 'get')
+        .mockImplementation(() => Promise.resolve({}));
+
+      await lxdService.getCloneStatus(
+        { serverUrl: 'https://destination.com' } as Remote,
+        '1'
+      );
+      expect(axiosRequest.get).toHaveBeenCalledWith(
+        'https://destination.com/1.0/operations/1/wait'
+      );
     });
-
-    describe('cloneImage', () => {
-
-        it('should correctly call the sourceimage factory ', async () => {
-            jest.spyOn(sourceImageFactory, 'entityToDto').mockImplementation(() => sourceImageMock);
-            jest.spyOn(request, 'post').mockImplementation((param) => ({ metadata: { id: '1' } }));
-            jest.spyOn(request, 'get').mockImplementation((param) => { });
-
-            await lxdService.cloneImage(image, remote1, remote2);
-            expect(request.post).toHaveBeenCalledWith({
-                url: 'https://destination.com/1.0/images',
-                json: {},
-                cert: certBuffer,
-                key: keyBuffer,
-                rejectUnauthorized: false
-            });
-            expect(sourceImageFactory.entityToDto).toHaveBeenCalledWith(image, remote1, remote2);
-        });
-    });
-
-    describe('getCloneStatus', () => {
-
-        it('should correctly create requests ', async () => {
-            jest.spyOn(request, 'post').mockImplementation((param) => ({ metadata: { id: '/1' } }));
-            jest.spyOn(request, 'get').mockImplementation((param) => { });
-
-            await lxdService.getCloneStatus({ serverUrl: 'https://destination.com' } as Remote, '1');
-            expect(request.get).toHaveBeenCalledWith({
-                url: 'https://destination.com/1.0/operations/1/wait',
-                cert: certBuffer,
-                key: keyBuffer,
-                rejectUnauthorized: false
-            });
-        });
-
-    });
+  });
 });
